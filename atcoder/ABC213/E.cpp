94e4c9ed6a201da9a4b586573b60ce78bc4a1fd8
nachiket kanore <nachiket.kanore@gmail.com>
2021-08-10 17:07:52 +0530

    Atcoder ABC213 E

---
+++ atcoder/ABC213/E.cpp
@@ -0,0 +1,86 @@
+/**
+ *    Author: Nachiket Kanore
+ *    Created: Tuesday 10 August 2021 04:55:51 PM IST
+**/
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <cassert>
+#include <array>
+#include <deque>
+#include <cstring>
+
+#define int int64_t
+#define sz(x) (int)(x.size())
+#define ALL(x) (x).begin(),(x).end()
+#define F0R(i,R) for(int i = (0); i < (R); ++i)
+#define FOR(i,L,R) for(int i = (L); i <= (R); ++i)
+
+using namespace std;
+
+struct ${ $() { ios::sync_with_stdio(0); cin.tie(0); } } $;
+
+template<class T> bool cmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool cmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; } 
+
+const int INF = 1e18;
+const int N = 505;
+const int dx[] = {0, 0, -1, 1};
+const int dy[] = {-1, 1, 0, 0};
+char mat[N][N];
+bool vis[N][N];
+int dist[N][N];
+int H, W;
+
+bool inside(int x, int y) {
+	return 1 <= x && x <= H && 1 <= y && y <= W;
+}
+
+int32_t main() {
+	cin >> H >> W;
+	FOR (i,1,H) {
+		FOR (j,1,W) {
+			cin >> mat[i][j];
+			dist[i][j] = INF;
+		}
+	}
+	deque<pair<int,int>> Q;
+	dist[1][1] = 0;
+	Q.push_back(make_pair(1, 1));
+
+	while (!Q.empty()) {
+		auto [x, y] = Q.front();
+		Q.pop_front();
+		if (vis[x][y]) continue;
+		vis[x][y] = true;
+
+		F0R (id,4) {
+			int nx = x + dx[id];
+			int ny = y + dy[id];
+			if (!inside(nx, ny)) continue;
+			if (dist[nx][ny] > dist[x][y] && mat[nx][ny] == '.') {
+				dist[nx][ny] = dist[x][y];
+				Q.push_front(make_pair(nx, ny));
+			}
+		}
+		
+		for (int cx = -2; cx <= 2; cx++) {
+			for (int cy = -2; cy <= 2; cy++) {
+				if (abs(cx) + abs(cy) >= 4) continue;
+				int nx = x + cx;
+				int ny = y + cy;
+				if (!inside(nx, ny)) continue;
+				if (dist[nx][ny] > dist[x][y]) {
+					dist[nx][ny] = dist[x][y] + 1;
+					Q.push_back(make_pair(nx, ny));
+				}
+			}
+		}
+	}
+	int answer = dist[H][W];
+	assert(answer ^ INF);
+	cout << answer << '\n';
+}
