fc0eb10723f7625cf86d7065468e0a2180ccd516
nachiket kanore <nachiket.kanore@gmail.com>
2022-02-04 17:00:11 +0530

    codechef feb long

---
+++ feb22a/binbasbasic.cpp
@@ -0,0 +1,43 @@
+/**
+ *    Author: Nachiket Kanore
+ *    Created: Friday 04 February 2022 03:15:04 PM IST
+**/
+#include <bits/stdc++.h>
+#define int int64_t
+using namespace std;
+
+int32_t main() {
+	int T; cin >> T;
+	while (T--) {
+		int N, K; string S;
+		cin >> N >> K >> S;
+		auto get_bad = [&]() {
+			int bad = 0;
+			int L = 0, R = N - 1;
+			while (L < R) {
+				bad += S[L++] != S[R--];
+			}
+			return bad;
+		};
+		int bad = get_bad();
+		if (N % 2 == 0) {
+			if (K < bad) {
+				cout << "NO\n";
+			} else {
+				K -= bad;
+				if (K % 2 == 0) {
+					cout << "YES\n";
+				} else {
+					cout << "NO\n";
+				}
+			}
+		} else {
+			if (K < bad) {
+				cout << "NO\n";
+			} else {
+				cout << "YES\n";
+			}
+		}
+	}
+	return 0;
+}
---
+++ feb22a/bitblend.cpp
@@ -0,0 +1,81 @@
+/**
+ *    Author: Nachiket Kanore
+ *    Created: Friday 04 February 2022 03:55:51 PM IST
+**/
+#include <bits/stdc++.h>
+#define int int64_t
+using namespace std;
+static std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
+
+int32_t main() {
+	int T = 100;
+	cin >> T;
+	while (T--) {
+		int N; cin >> N;
+		int B[N], cnt[2]; cnt[0] = cnt[1] = 0;
+		for (int i = 0; i < N; i++) {
+			cin >> B[i];
+			B[i] %= 2;
+			cnt[B[i]]++;
+		}
+		if (cnt[0] >= 1 && cnt[1] >= 1) {
+			vector<pair<int,int>> ans;
+			int got = 1e9;
+			for (int start = 0; start < 2; start++) {
+				vector<pair<int,int>> best;
+				int A[N]; for (int i = 0; i < N; i++) A[i] = B[i];
+				int one_pos = -1;
+				int make[N];
+				for (int i = 0; i < N; i++) {
+					make[i] = (start + i) % 2;
+					if (A[i] == 1 && A[i] == make[i]) {
+						one_pos = i;
+					}
+				}
+				if (one_pos == -1) {
+					for (int i = 0; i < N; i++) {
+						if (A[i] == 1) {
+							one_pos = i;
+						}
+					}
+				}
+				assert(~one_pos);
+				for (int i = 0; i < N; i++) {
+					if (i == one_pos) continue;
+					if (make[i] ^ A[i]) {
+						best.push_back(make_pair(i, one_pos));
+						A[i] ^= 1;
+					}
+				}
+				if (make[one_pos] ^ A[one_pos]) {
+					int other_one_pos = -1;
+					for (int i = 0; i < N; i++) {
+						if (A[i] == 1 && i != one_pos) {
+							other_one_pos = i;
+							break;
+						}
+					}
+					assert(~other_one_pos);
+					assert(make[other_one_pos] == A[other_one_pos]);
+					best.push_back(make_pair(one_pos, other_one_pos));
+					A[one_pos] ^= A[other_one_pos];
+				}
+				for (int i = 0; i < N; i++) {
+					assert(A[i] == make[i]);
+				}
+				if (got > (int)best.size()) {
+					got = best.size();
+					ans = best;
+				}
+			}
+			cout << ans.size() << '\n';
+			for (auto pair: ans) {
+				cout << pair.first + 1 << ' ' << pair.second + 1 << '\n';
+			}
+
+		} else {
+			cout << "-1\n";
+		}
+	}
+	return 0;
+}
