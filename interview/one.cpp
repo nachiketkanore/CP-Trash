6abc51d58897cd78d4ddb23729bc3ad02cf25de6
nachiket kanore <nachiket.kanore@gmail.com>
2022-03-07 17:33:57 +0530

    interview problems

---
+++ interview/one.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string magiclamp(vector<int> wish) {
+	sort(wish.begin(), wish.end(), [&](int first, int second) {
+		string A = to_string(first), B = to_string(second);
+		return A + B > B + A;
+	});
+	string ans;
+	for (int a: wish) {
+		ans += to_string(a);
+	}
+	reverse(ans.begin(), ans.end());
+	while ((int)ans.size() > 1 && ans.back() == '0') {
+		ans.pop_back();
+	}
+	reverse(ans.begin(), ans.end());
+	return ans;
+}
+
+int main() {
+	// vector<int> A{ 13, 65, 9, 20 };
+	vector<int> A{0, 0, 0, 0};
+	cout << magiclamp(A);
+	return 0;
+}
---
+++ interview/two.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(vector<int>& nums, int l, int r, vector<vector<int>> &dp){
+	if(l >= r) return 0;
+	if(dp[l][r] != -1) return dp[l][r];
+
+	int ans = 0;
+
+	for(int i = l; i<r; i++){
+		int temp = solve(nums, l, i, dp) + solve(nums, i+1, r, dp) + nums[l-1]*nums[i]*nums[r];
+		ans = max(ans, temp);
+	}
+	return dp[l][r] = ans;
+}
+
+int coins(vector<int>& zombie)
+{
+	zombie.insert(zombie.begin(), 1);
+	zombie.push_back(1);
+	int n = zombie.size();
+
+	vector<vector<int>> dp(n, vector<int>(n, -1));
+	return solve(zombie, 1, n-1, dp);
+}
+
+int main() {
+	vector<int> A{ 5, 1, 3, 9 };
+	cout << coins(A);
+	return 0;
+}
