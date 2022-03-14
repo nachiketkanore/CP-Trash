acd8ffac51f291d36bd92694c097b835a8de3faa
nachiket kanore <nachiket.kanore@gmail.com>
2022-03-14 19:32:32 +0530

    back to cp?
    codeforces Div.3 problems

---
+++ div3/B.cpp
@@ -0,0 +1,42 @@
+/**
+ *    Author: Nachiket Kanore
+ *    Created: Monday 14 March 2022 07:05:51 PM IST
+**/
+#include <bits/stdc++.h>
+#define int int64_t
+using namespace std;
+
+
+int L, R;
+
+int f(int x, int a) {
+	if (a <= 0 || !(L <= x && x <= R)) return 0;
+	return (x / a) + (x % a);
+}
+
+int32_t main() {
+	int T;
+	cin >> T;
+	while (T--) {
+		int a;
+		cin >> L >> R >> a;
+		int ans = max(f(L, a), f(R, a));
+		for (int d = 1; d * d <= a; d++) {
+			if (a % d == 0) {
+				int one = d, two = a / d;
+				ans = max(ans, f(one, a));
+				ans = max(ans, f(one - 1, a));
+				ans = max(ans, f(one + 1, a));
+				ans = max(ans, f(two, a));
+				ans = max(ans, f(two - 1, a));
+				ans = max(ans, f(two + 1, a));
+			} else {
+				ans = max(ans, f(d - 1, a));
+				ans = max(ans, f(d, a));
+				ans = max(ans, f(d + 1, a));
+			}
+		}
+		cout << ans << '\n';
+	}
+	return 0;
+}
---
+++ div3/C.cpp
@@ -0,0 +1,48 @@
+/**
+ *    Author: Nachiket Kanore
+ *    Created: Monday 14 March 2022 07:20:38 PM IST
+**/
+#include <bits/stdc++.h>
+#define int int64_t
+using namespace std;
+
+struct Point {
+	int id, x, w;
+
+	Point(int id, int x, int w): id(id), x(x), w(w) {}
+};
+
+int32_t main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int T;
+	cin >> T;
+	while (T--) {
+		int N, M;
+		cin >> N >> M;
+		assert(2 * N <= M);
+		vector<Point> pts;
+		for (int i = 1; i <= M; i++) {
+			int x, w;
+			cin >> x >> w;
+			pts.push_back(Point(i, x, w));
+		}
+		sort(pts.begin(), pts.end(), [&](const Point &one, const Point &two) {
+			return one.w < two.w;
+		});
+		int cost = 0;
+		for (int i = 0; i < 2*N; i++) {
+			cost += pts[i].w;
+		}
+		cout << cost << '\n';
+		sort(pts.begin(), pts.begin() + 2*N, [&](const Point &one, const Point &two) {
+			return one.x < two.x;
+		});
+		int L = 0, R = 2 * N - 1;
+		while (L < R) {
+			cout << pts[L].id << ' ' << pts[R].id << '\n';
+			L++, R--;
+		}
+	}
+	return 0;
+}
