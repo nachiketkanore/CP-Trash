f0f135ce65a0ac4743dfc4c65e104508d5a5a3d9
nachiket kanore <nachiket.kanore@gmail.com>
2022-06-02 12:42:44 +0530

    2100 rated problem solved

---
+++ codeforces/1555E/sol.cpp
@@ -0,0 +1,223 @@
+/**
+ *    Author: Nachiket Kanore
+ *    Created: Thursday 02 June 2022 12:05:55 PM IST
+ **/
+#include "bits/stdc++.h"
+using namespace std;
+
+#ifdef DEBUG
+#include "debug.h"
+#else
+#define see(...) ;
+#endif
+
+#define int int64_t
+#define sz(x) (int)(x.size())
+#define ALL(x) (x).begin(), (x).end()
+#define F0R(i, R) for (int i = (0); i < (R); ++i)
+#define FOR(i, L, R) for (int i = (L); i <= (R); ++i)
+
+const int INF = 1e18;
+
+class node {
+	public:
+	static const int inf = 1e18;
+	int mn, mx, sum;
+
+	node(int _mn, int _mx, int _sum) {
+		mn = _mn;
+		mx = _mx;
+		sum = _sum;
+	}
+
+	node() {
+		mn = inf;
+		mx = -inf;
+		sum = 0;
+	}
+};
+
+struct segtree {
+	int N;
+	vector<node> st;
+	vector<bool> cLazy;
+	vector<int> lazy;
+
+	void init(int n) {
+		N = n;
+		st.resize((N << 2) + 2);
+		cLazy.assign((N << 2) + 2, false);
+		lazy.assign((N << 2) + 2, 0);
+		build(1, 1, N);
+	}
+
+	// Write reqd merge functions
+	void merge(node& ret, node& left, node& right) {
+		ret.mn = min(left.mn, right.mn);
+		ret.mx = max(left.mx, right.mx);
+		ret.sum = left.sum + right.sum;
+	}
+
+	// Handle lazy propagation appriopriately
+	void propagate(int u, int L, int R) {
+		// Propagate down?
+		if (L != R) {
+			cLazy[u * 2] = 1;
+			cLazy[u * 2 + 1] = 1;
+
+			// Default range update operation is :
+			// adding lazy[u] to all elements in range [...]
+			lazy[u * 2] += lazy[u];
+			lazy[u * 2 + 1] += lazy[u];
+		}
+
+		// Updating the range [L, R] with lazy[u]
+		{
+			// Default range update operation is :
+			// adding lazy[u] to all elements in range [...]
+			st[u].sum += (R - L + 1) * lazy[u];
+			st[u].mx += lazy[u];
+			st[u].mn += lazy[u];
+		}
+		cLazy[u] = 0;
+		lazy[u] = 0;
+	}
+
+	void build(int u, int L, int R) {
+		if (L == R) {
+			// Leaf value
+			st[u] = node(0, 0, 0);
+			return;
+		}
+		int M = (L + R) / 2;
+		build(u * 2, L, M);
+		build(u * 2 + 1, M + 1, R);
+		merge(st[u], st[u * 2], st[u * 2 + 1]);
+	}
+
+	node Query(int u, int L, int R, int i, int j) {
+		if (cLazy[u])
+			propagate(u, L, R);
+		if (j < L || i > R)
+			return node();
+		if (i <= L && R <= j)
+			return st[u];
+		int M = (L + R) / 2;
+		node left = Query(u * 2, L, M, i, j);
+		node right = Query(u * 2 + 1, M + 1, R, i, j);
+		node ret;
+		merge(ret, left, right);
+		return ret;
+	}
+
+	node pQuery(int u, int L, int R, int pos) {
+		if (cLazy[u])
+			propagate(u, L, R);
+		if (L == R)
+			return st[u];
+		int M = (L + R) / 2;
+		if (pos <= M)
+			return pQuery(u * 2, L, M, pos);
+		else
+			return pQuery(u * 2 + 1, M + 1, R, pos);
+	}
+
+	void Update(int u, int L, int R, int i, int j, int val) {
+		if (cLazy[u])
+			propagate(u, L, R);
+		if (j < L || i > R)
+			return;
+		if (i <= L && R <= j) {
+			// Default range update operation is :
+			// adding val to all elements in range [...]
+			cLazy[u] = 1;
+			lazy[u] = val;
+			propagate(u, L, R);
+			return;
+		}
+		int M = (L + R) / 2;
+		Update(u * 2, L, M, i, j, val);
+		Update(u * 2 + 1, M + 1, R, i, j, val);
+		merge(st[u], st[u * 2], st[u * 2 + 1]);
+	}
+
+	void pUpdate(int u, int L, int R, int pos, int val) {
+		if (cLazy[u])
+			propagate(u, L, R);
+		if (L == R) {
+			cLazy[u] = 1;
+			lazy[u] = val;
+			propagate(u, L, R);
+			return;
+		}
+		int M = (L + R) / 2;
+		if (pos <= M)
+			pUpdate(u * 2, L, M, pos, val);
+		else
+			pUpdate(u * 2 + 1, M + 1, R, pos, val);
+		merge(st[u], st[u * 2], st[u * 2 + 1]);
+	}
+
+	node query(int pos) {
+		return pQuery(1, 1, N, pos);
+	}
+
+	node query(int l, int r) {
+		return Query(1, 1, N, l, r);
+	}
+
+	void update(int pos, int val) {
+		pUpdate(1, 1, N, pos, val);
+	}
+
+	void update(int l, int r, int val) {
+		Update(1, 1, N, l, r, val);
+	}
+};
+
+int32_t main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N, M;
+	cin >> N >> M;
+
+	vector<array<int, 3>> A(N);
+	segtree segs;
+	segs.init(M - 1);
+
+	for (auto& [w, L, R] : A)
+		cin >> L >> R >> w;
+
+	sort(ALL(A));
+
+	int ans = INF;
+
+	auto gap_exists = [&]() -> bool {
+		int gap = segs.query(1, M - 1).mn;
+		return gap == 0;
+	};
+
+	auto add_segment = [&](int L, int R) -> void { segs.update(L, R - 1, +1); };
+
+	auto remove_segment = [&](int L, int R) -> void { segs.update(L, R - 1, -1); };
+
+	for (int i = 0, j = 0; i < N; i++) {
+		while (j < N && gap_exists()) {
+			add_segment(A[j][1], A[j][2]);
+			j++;
+		}
+
+		if (!gap_exists()) {
+			assert(i <= j - 1);
+			ans = min(ans, A[j - 1][0] - A[i][0]);
+		}
+
+		remove_segment(A[i][1], A[i][2]);
+	}
+
+	assert(ans ^ INF);
+	cout << ans << '\n';
+
+	return 0;
+}
