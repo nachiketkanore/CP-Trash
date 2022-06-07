f48f7e1b9b00136d431f2e44f2a9f56da5c3bd1c
nachiket kanore <nachiket.kanore@gmail.com>
2022-06-07 13:11:15 +0530

    CF practice

---
+++ codeforces/1525E/sol.cpp
@@ -0,0 +1,155 @@
+/**
+ *    Author: Nachiket Kanore
+ *    Created: Tuesday 07 June 2022 12:46:06 PM IST
+ **/
+#include "bits/stdc++.h"
+using namespace std;
+
+#ifdef DEBUG
+#include "debug.h"
+#else
+#define see(...) ;
+#endif
+
+#define int int64_t
+#define sz(x) (int)(x.size())
+#define ALL(x) (x).begin(), (x).end()
+#define F0R(i, R) for (int i = (0); i < (R); ++i)
+#define FOR(i, L, R) for (int i = (L); i <= (R); ++i)
+
+const int MOD = 998244353;
+struct mi {
+	using ll = long long;
+	ll v;
+	explicit operator ll() const {
+		return v;
+	}
+	mi() {
+		v = 0;
+	}
+	mi(ll _v) {
+		v = (-MOD < _v && _v < MOD) ? _v : _v % MOD;
+		if (v < 0)
+			v += MOD;
+	}
+	friend bool operator==(const mi& a, const mi& b) {
+		return a.v == b.v;
+	}
+	friend bool operator!=(const mi& a, const mi& b) {
+		return !(a == b);
+	}
+	friend bool operator<(const mi& a, const mi& b) {
+		return a.v < b.v;
+	}
+	mi& operator+=(const mi& m) {
+		if ((v += m.v) >= MOD)
+			v -= MOD;
+		return *this;
+	}
+	mi& operator-=(const mi& m) {
+		if ((v -= m.v) < 0)
+			v += MOD;
+		return *this;
+	}
+	mi& operator*=(const mi& m) {
+		v = v * m.v % MOD;
+		return *this;
+	}
+	mi& operator/=(const mi& m) {
+		return (*this) *= inv(m);
+	}
+
+	friend mi pow(mi a, ll p) {
+		mi ans = 1;
+		assert(p >= 0);
+		for (; p; p /= 2, a *= a)
+			if (p & 1)
+				ans *= a;
+		return ans;
+	}
+	friend mi inv(const mi& a) {
+		assert(a.v != 0);
+		return pow(a, MOD - 2);
+	}
+
+	mi operator-() const {
+		return mi(-v);
+	}
+	mi& operator++() {
+		return *this += 1;
+	}
+	mi& operator--() {
+		return *this -= 1;
+	}
+	mi operator++(int32_t) {
+		mi temp;
+		temp.v = v++;
+		return temp;
+	}
+	mi operator--(int32_t) {
+		mi temp;
+		temp.v = v--;
+		return temp;
+	}
+	friend mi operator+(mi a, const mi& b) {
+		return a += b;
+	}
+	friend mi operator-(mi a, const mi& b) {
+		return a -= b;
+	}
+	friend mi operator*(mi a, const mi& b) {
+		return a *= b;
+	}
+	friend mi operator/(mi a, const mi& b) {
+		return a /= b;
+	}
+	friend ostream& operator<<(ostream& os, const mi& m) {
+		os << m.v;
+		return os;
+	}
+	friend istream& operator>>(istream& is, mi& m) {
+		ll x;
+		is >> x;
+		m.v = x;
+		return is;
+	}
+};
+
+int N, M;
+int dist[21][50005];
+
+int32_t main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> N >> M;
+	FOR(i, 1, N) FOR(j, 1, M) cin >> dist[i][j];
+
+	mi E = 0;
+	mi n_fact = 1;
+	FOR(i, 1, N) n_fact *= i;
+
+	FOR(pt, 1, M) {
+		int choices[N + 5];
+		memset(choices, 0, sizeof(choices));
+
+		FOR(city, 1, N) {
+			int upto = N - dist[city][pt] + 2;
+			if (upto >= 1 && upto <= N)
+				choices[upto]++;
+		}
+
+		FOR(i, 2, N) choices[i] += choices[i - 1];
+
+		mi bad_prob = 1;
+		FOR(i, 1, N) {
+			bad_prob *= max(int(0), choices[i] - i + 1);
+		}
+		bad_prob /= n_fact;
+
+		E += mi(1 - bad_prob);
+	}
+
+	cout << E << '\n';
+	return 0;
+}
