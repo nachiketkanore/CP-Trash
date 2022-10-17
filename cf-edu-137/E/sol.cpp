25a2f6167c57b4201a18a1c5516ad45fd68f2726
Nachiket Kanore <nachiket.kanore@gmail.com>
2022-10-17 22:13:41 +0530

    codeforces educational round participation

---
+++ cf-edu-137/E/sol.cpp
@@ -0,0 +1,38 @@
+/**
+ *    Author: Nachiket Kanore
+ *    Created: Monday 17 October 2022 09:54:42 PM IST
+ **/
+#include "bits/stdc++.h"
+using namespace std;
+
+#ifdef DEBUG
+#include "debug.h"
+#else
+#define see(...) ;
+#endif
+
+#define int int64_t
+#define sz(x) (int)(x.size())
+#define ALL(x) (x).begin(), (x).end()
+#define F0R(i, R) for (int i = (0); i < (R); ++i)
+#define FOR(i, L, R) for (int i = (L); i <= (R); ++i)
+
+int p1, t1, p2, t2, h, s;
+
+void solve() {
+	cin >> p1 >> t1 >> p2 >> t2 >> h >> s;
+}
+
+int32_t main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int T;
+	cin >> T;
+
+	while (T--) {
+		solve();
+	}
+
+	return 0;
+}
