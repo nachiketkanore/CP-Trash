b8d57c202ce2415abeb918d513bd8e017acd6df6
Nachiket Kanore <nachiket.kanore@gmail.com>
2022-11-06 23:57:26 +0530

    upsolved problem D from last CF round
    takeaways:
    - make equations
    - simplify them
    - think based on expected complexity of the solution
    - simple observations are important

---
+++ cf/round-codeton/D/sol.cpp
@@ -0,0 +1,178 @@
+/**
+ *    Author: Nachiket Kanore
+ *    Created: Sunday 06 November 2022 11:34:38 PM IST
+ **/
+#include "bits/stdc++.h"
+using namespace std;
+
+#ifdef DEBUG
+#include "debug.h"
+#else
+#define see(...) ;
+#endif
+
+#define int int64_t
+#define sz(x) (int)(x.size())
+#define ALL(x) (x).begin(), (x).end()
+#define F0R(i, R) for (int i = (0); i < (R); ++i)
+#define FOR(i, L, R) for (int i = (L); i <= (R); ++i)
+
+const int MOD = 998244353;
+struct mi {
+	using ll = long long;
+	ll v;
+	explicit operator ll() const {
+		return v;
+	}
+	mi() {
+		v = 0;
+	}
+	mi(ll _v) {
+		v = (-MOD < _v && _v < MOD) ? _v : _v % MOD;
+		if (v < 0)
+			v += MOD;
+	}
+	friend bool operator==(const mi& a, const mi& b) {
+		return a.v == b.v;
+	}
+	friend bool operator!=(const mi& a, const mi& b) {
+		return !(a == b);
+	}
+	friend bool operator<(const mi& a, const mi& b) {
+		return a.v < b.v;
+	}
+	mi& operator+=(const mi& m) {
+		if ((v += m.v) >= MOD)
+			v -= MOD;
+		return *this;
+	}
+	mi& operator-=(const mi& m) {
+		if ((v -= m.v) < 0)
+			v += MOD;
+		return *this;
+	}
+	mi& operator*=(const mi& m) {
+		v = v * m.v % MOD;
+		return *this;
+	}
+	mi& operator/=(const mi& m) {
+		return (*this) *= inv(m);
+	}
+
+	friend mi pow(mi a, ll p) {
+		mi ans = 1;
+		assert(p >= 0);
+		for (; p; p /= 2, a *= a)
+			if (p & 1)
+				ans *= a;
+		return ans;
+	}
+	friend mi inv(const mi& a) {
+		assert(a.v != 0);
+		return pow(a, MOD - 2);
+	}
+
+	mi operator-() const {
+		return mi(-v);
+	}
+	mi& operator++() {
+		return *this += 1;
+	}
+	mi& operator--() {
+		return *this -= 1;
+	}
+	mi operator++(int32_t) {
+		mi temp;
+		temp.v = v++;
+		return temp;
+	}
+	mi operator--(int32_t) {
+		mi temp;
+		temp.v = v--;
+		return temp;
+	}
+	friend mi operator+(mi a, const mi& b) {
+		return a += b;
+	}
+	friend mi operator-(mi a, const mi& b) {
+		return a -= b;
+	}
+	friend mi operator*(mi a, const mi& b) {
+		return a *= b;
+	}
+	friend mi operator/(mi a, const mi& b) {
+		return a /= b;
+	}
+	friend ostream& operator<<(ostream& os, const mi& m) {
+		os << m.v;
+		return os;
+	}
+	friend istream& operator>>(istream& is, mi& m) {
+		ll x;
+		is >> x;
+		m.v = x;
+		return is;
+	}
+};
+
+vector<int> prime_factors(int u) {
+	vector<int> ret;
+	for (int d = 2; d * d <= u; ++d) {
+		if (u % d == 0) {
+			while (u % d == 0)
+				u /= d;
+			ret.push_back(d);
+		}
+	}
+	if (u > 1)
+		ret.push_back(u);
+	return ret;
+}
+
+void solve() {
+	int N, M;
+	cin >> N >> M;
+	int A[N];
+	F0R(i, N) cin >> A[i];
+	mi ans = 1;
+
+	auto contribution = [&](int with, int limit) -> mi {
+		vector<int> pfs = prime_factors(with);
+		mi ret = 0;
+		F0R(mask, 1 << sz(pfs)) {
+			int term = limit;
+			int sign = 1;
+			F0R(i, sz(pfs)) {
+				if (mask >> i & 1)
+					term /= pfs[i], sign *= -1;
+			}
+			ret += term * sign;
+		}
+		return ret;
+	};
+
+	for (int i = 1; i < N; ++i) {
+		if (A[i - 1] % A[i] != 0) {
+			ans = 0;
+			break;
+		} else {
+			ans *= contribution(A[i - 1] / A[i], M / A[i]);
+		}
+	}
+
+	cout << ans << '\n';
+}
+
+int32_t main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int T;
+	cin >> T;
+
+	while (T--) {
+		solve();
+	}
+
+	return 0;
+}
