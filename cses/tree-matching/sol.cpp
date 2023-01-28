c685aa73a4a9f4312f160e7b8c764d1f58c9ca25
Nachiket Kanore <nachiket.kanore@gmail.com>
2023-01-28 18:32:04 +0530

    final AC solution(recursive top down DP) for tree-matching

--- cses/tree-matching/sol.cpp
+++ cses/tree-matching/sol.cpp
@@ -14,9 +14,13 @@ using namespace std;
 const int MX = 2e5 + 5;
 int N;
 vector<int> g[MX];
+int dp[2][MX];
 
 int go(int curr, int parent, bool counted_already) {
-	int ans = 0;
+	int& ans = dp[counted_already][curr];
+	if (~ans)
+		return ans;
+	ans = 0;
 	int block2 = 0;
 	// maybe precompute this
 	for (const int& child : g[curr])
@@ -46,18 +50,18 @@ int go(int curr, int parent, bool counted_already) {
 			}
 
 			// choice 2: don't use this edge
-			{
-				/* block2 */
-				// this block just computes go($child, curr, false) over all children of node `curr`
-				// int c2 = 0 + go(child, curr, false);
-				// int add = 0; // any way to optimize computing `add`?
-				// for (int other_child : g[curr]) {
-				// 	if (other_child != parent && other_child != child) {
-				// 		add += go(other_child, curr, false);
-				// 	}
-				// }
-				// ans = max(ans, c2 + add);
-			}
+			// {
+			/* block2 */
+			// this block just computes go($child, curr, false) over all children of node `curr`
+			// int c2 = 0 + go(child, curr, false);
+			// int add = 0; // any way to optimize computing `add`?
+			// for (int other_child : g[curr]) {
+			// 	if (other_child != parent && other_child != child) {
+			// 		add += go(other_child, curr, false);
+			// 	}
+			// }
+			// ans = max(ans, c2 + add);
+			// }
 			// block2 += go(child, curr, false);
 		}
 	}
@@ -69,6 +73,7 @@ int32_t main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
+	memset(dp, -1, sizeof(dp));
 	cin >> N;
 	for (int i = 1, u, v; i < N; i++) {
 		cin >> u >> v;
