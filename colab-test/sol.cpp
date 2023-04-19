a59025e522e8ee516cc6aaad427b9e1db141b392
Nachiket Kanore <nachiket.kanore@gmail.com>
2023-04-19 12:01:49 +0530

    I/O automation script for retracer contests on google colab

---
+++ colab-test/sol.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define int int64_t
+using namespace std;
+
+int32_t main() {
+	int x;
+	cin >> x;
+	if (x % 2 == 0) {
+		cout << "YES\n";
+	} else {
+		cout << "NO\n";
+	}
+	return 0;
+}
+
