235d344192e4dca57ea65f4d7c2183546d4dad24
Nachiket Kanore <nachiket.kanore@gmail.com>
2024-02-04 22:52:33 +0530

    solved atcoder problem on dynamic programming

---
+++ atcoder/abc321_f/sol.cpp
@@ -0,0 +1,148 @@
+/**
+ *    Author: Nachiket Kanore
+ *    Created: Sunday 04 February 2024 10:31:54 PM IST
+ **/
+#include "bits/stdc++.h"
+using namespace std;
+
+#ifdef DEBUG
+#include "debug.h"
+#else
+#define see(...) ;
+#endif
+
+#define int int64_t
+#define sz(x) (int)(x.size())
+#define ALL(x) (x).begin(), (x).end()
+#define F0R(i, R) for (int i = (0); i < (R); ++i)
+#define FOR(i, L, R) for (int i = (L); i <= (R); ++i)
+
+const int MOD = 998244353;
+struct mi {
+	using ll = long long;
+	ll v;
+	explicit operator ll() const {
+		return v;
+	}
+	mi() {
+		v = 0;
+	}
+	mi(ll _v) {
+		v = (-MOD < _v && _v < MOD) ? _v : _v % MOD;
+		if (v < 0)
+			v += MOD;
+	}
+	friend bool operator==(const mi& a, const mi& b) {
+		return a.v == b.v;
+	}
+	friend bool operator!=(const mi& a, const mi& b) {
+		return !(a == b);
+	}
+	friend bool operator<(const mi& a, const mi& b) {
+		return a.v < b.v;
+	}
+	mi& operator+=(const mi& m) {
+		if ((v += m.v) >= MOD)
+			v -= MOD;
+		return *this;
+	}
+	mi& operator-=(const mi& m) {
+		if ((v -= m.v) < 0)
+			v += MOD;
+		return *this;
+	}
+	mi& operator*=(const mi& m) {
+		v = v * m.v % MOD;
+		return *this;
+	}
+	mi& operator/=(const mi& m) {
+		return (*this) *= inv(m);
+	}
+
+	friend mi pow(mi a, ll p) {
+		mi ans = 1;
+		assert(p >= 0);
+		for (; p; p /= 2, a *= a)
+			if (p & 1)
+				ans *= a;
+		return ans;
+	}
+	friend mi inv(const mi& a) {
+		assert(a.v != 0);
+		return pow(a, MOD - 2);
+	}
+
+	mi operator-() const {
+		return mi(-v);
+	}
+	mi& operator++() {
+		return *this += 1;
+	}
+	mi& operator--() {
+		return *this -= 1;
+	}
+	mi operator++(int32_t) {
+		mi temp;
+		temp.v = v++;
+		return temp;
+	}
+	mi operator--(int32_t) {
+		mi temp;
+		temp.v = v--;
+		return temp;
+	}
+	friend mi operator+(mi a, const mi& b) {
+		return a += b;
+	}
+	friend mi operator-(mi a, const mi& b) {
+		return a -= b;
+	}
+	friend mi operator*(mi a, const mi& b) {
+		return a *= b;
+	}
+	friend mi operator/(mi a, const mi& b) {
+		return a /= b;
+	}
+	friend ostream& operator<<(ostream& os, const mi& m) {
+		os << m.v;
+		return os;
+	}
+	friend istream& operator>>(istream& is, mi& m) {
+		ll x;
+		is >> x;
+		m.v = x;
+		return is;
+	}
+};
+mi dp[100000];
+
+int32_t main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int Q, K;
+	cin >> Q >> K;
+	dp[0] = 1;
+	while (Q--) {
+		char ch;
+		int x;
+		cin >> ch >> x;
+		if (ch == '+') {
+			for (int prev = K; prev >= 0; prev--) {
+				if (dp[prev].v > 0)
+					dp[prev + x] += dp[prev];
+			}
+		} else if (ch == '-') {
+			dp[x]--;
+			FOR(prev, 1, K) {
+				if (dp[prev].v > 0)
+					dp[prev + x] -= dp[prev];
+			}
+		} else {
+			assert(false);
+		}
+		cout << dp[K] << '\n';
+	}
+
+	return 0;
+}
